ed30db1d0b897d11e90fc6adb822084abca373f5
gabriel <gabriel.g.robin@airbus.com>
2024-07-12 17:04:13 +0200

    align version

--- ipyvuetable/__init__.py
+++ ipyvuetable/__init__.py
@@ -8,5 +8,5 @@ from ipyvuetable.table import Table
 # load css classes
 display(HTML(f"<style>{(Path(__file__).parent / 'custom.css').read_text()}</style>"))
 
-__version__ = "0.3.0"
+__version__ = "0.5.0"
 __all__ = ["Table", "EditingTable"]
