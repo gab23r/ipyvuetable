e1f3446069eb5442ce732c4f121f08456b2302f2
gabriel <gabriel.g.robin@airbus.com>
2024-08-28 11:37:29 +0200

    bump

--- ipyvuetable/__init__.py
+++ ipyvuetable/__init__.py
@@ -8,5 +8,5 @@ from ipyvuetable.table import Table
 # load css classes
 display(HTML(f"<style>{(Path(__file__).parent / 'custom.css').read_text()}</style>"))
 
-__version__ = "0.7.11"
+__version__ = "0.7.12"
 __all__ = ["Table", "EditingTable"]
